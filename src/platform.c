20fc927cee71927dfd5408cbcd7cbf3d0b54a44d
Nael Iskandarani <nael.nanomano@gmail.com>
2023-11-07 23:35:10 -0600

    Move event handling to input processor

--- src/platform.c
+++ src/platform.c
@@ -1,4 +1,5 @@
 #include "platform.h"
+#include <X11/Xlib.h>
 #include <stdio.h>
 
 #define KEY_ESCAPE 9
@@ -43,9 +44,8 @@ void platform_create_window(struct window *window)
     uint16_t window_x = (screen_width - (screen_width / 2)) / 2;
     uint16_t window_y = (screen_height - (screen_height / 2)) / 2;
 
-    /* Create window the size of half the screen */
     window->w = XCreateSimpleWindow(window->dpy, RootWindow(window->dpy, window->scr), window_x, window_y,
-                                    screen_width / 2, screen_height / 2, 0, window->black, window->black);
+                                    screen_width / 3, (screen_width / 3) / 2, 0, window->black, window->black);
 
     XStoreName(window->dpy, window->w, "Cilly");
     XMapWindow(window->dpy, window->w);
@@ -73,128 +73,133 @@ void platform_destroy_window(struct window *window)
 uint8_t platform_process_input(struct window *window, uint8_t *keypad)
 {
     uint8_t running = 1;
-    /* Set on key press */
-    switch (window->e.type)
+    while (XPending(window->dpy))
     {
-    case KeyPress: {
-        switch (window->e.xkey.keycode)
+        XNextEvent(window->dpy, &window->e);
+
+        /* Set on key press */
+        switch (window->e.type)
         {
-        case KEY_1:
-            keypad[1] = 1;
-            break;
-        case KEY_2:
-            keypad[2] = 1;
-            break;
-        case KEY_3:
-            keypad[3] = 1;
-            break;
-        case KEY_4:
-            keypad[0xC] = 1;
-            break;
+        case KeyPress: {
+            switch (window->e.xkey.keycode)
+            {
+            case KEY_1:
+                keypad[1] = 1;
+                break;
+            case KEY_2:
+                keypad[2] = 1;
+                break;
+            case KEY_3:
+                keypad[3] = 1;
+                break;
+            case KEY_4:
+                keypad[0xC] = 1;
+                break;
 
-        case KEY_Q:
-            keypad[4] = 1;
-            break;
-        case KEY_W:
-            keypad[5] = 1;
-            break;
-        case KEY_E:
-            keypad[6] = 1;
-            break;
-        case KEY_R:
-            keypad[0xD] = 1;
-            break;
+            case KEY_Q:
+                keypad[4] = 1;
+                break;
+            case KEY_W:
+                keypad[5] = 1;
+                break;
+            case KEY_E:
+                keypad[6] = 1;
+                break;
+            case KEY_R:
+                keypad[0xD] = 1;
+                break;
 
-        case KEY_A:
-            keypad[7] = 1;
-            break;
-        case KEY_S:
-            keypad[8] = 1;
-            break;
-        case KEY_D:
-            keypad[9] = 1;
-            break;
-        case KEY_F:
-            keypad[0xE] = 1;
-            break;
+            case KEY_A:
+                keypad[7] = 1;
+                break;
+            case KEY_S:
+                keypad[8] = 1;
+                break;
+            case KEY_D:
+                keypad[9] = 1;
+                break;
+            case KEY_F:
+                keypad[0xE] = 1;
+                break;
 
-        case KEY_Z:
-            keypad[0xA] = 1;
-            break;
-        case KEY_X:
-            keypad[0] = 1;
-            break;
-        case KEY_C:
-            keypad[0xB] = 1;
-            break;
-        case KEY_V:
-            keypad[0xF] = 1;
-            break;
+            case KEY_Z:
+                keypad[0xA] = 1;
+                break;
+            case KEY_X:
+                keypad[0] = 1;
+                break;
+            case KEY_C:
+                keypad[0xB] = 1;
+                break;
+            case KEY_V:
+                keypad[0xF] = 1;
+                break;
 
-        case KEY_ESCAPE:
-            running = 0;
-            break;
+            case KEY_ESCAPE:
+                running = 0;
+                break;
+            }
         }
-    }
-    break;
+        break;
 
-    case KeyRelease: {
-        switch (window->e.xkey.keycode)
-        {
-        case KEY_1:
-            keypad[1] = 0;
-            break;
-        case KEY_2:
-            keypad[2] = 0;
-            break;
-        case KEY_3:
-            keypad[3] = 0;
-            break;
-        case KEY_4:
-            keypad[0xC] = 0;
-            break;
+        case KeyRelease: {
+            switch (window->e.xkey.keycode)
+            {
+            case KEY_1:
+                keypad[1] = 0;
+                break;
+            case KEY_2:
+                keypad[2] = 0;
+                break;
+            case KEY_3:
+                keypad[3] = 0;
+                break;
+            case KEY_4:
+                keypad[0xC] = 0;
+                break;
 
-        case KEY_Q:
-            keypad[4] = 0;
-            break;
-        case KEY_W:
-            keypad[5] = 0;
-            break;
-        case KEY_E:
-            keypad[6] = 0;
-            break;
-        case KEY_R:
-            keypad[0xD] = 0;
-            break;
+            case KEY_Q:
+                keypad[4] = 0;
+                break;
+            case KEY_W:
+                keypad[5] = 0;
+                break;
+            case KEY_E:
+                keypad[6] = 0;
+                break;
+            case KEY_R:
+                keypad[0xD] = 0;
+                break;
 
-        case KEY_A:
-            keypad[7] = 0;
-            break;
-        case KEY_S:
-            keypad[8] = 0;
-            break;
-        case KEY_D:
-            keypad[9] = 0;
-            break;
-        case KEY_F:
-            keypad[0xE] = 0;
-            break;
+            case KEY_A:
+                keypad[7] = 0;
+                break;
+            case KEY_S:
+                keypad[8] = 0;
+                break;
+            case KEY_D:
+                keypad[9] = 0;
+                break;
+            case KEY_F:
+                keypad[0xE] = 0;
+                break;
 
-        case KEY_Z:
-            keypad[0xA] = 0;
-            break;
-        case KEY_X:
-            keypad[0] = 0;
-            break;
-        case KEY_C:
-            keypad[0xB] = 0;
-            break;
-        case KEY_V:
-            keypad[0xF] = 0;
-            break;
+            case KEY_Z:
+                keypad[0xA] = 0;
+                break;
+            case KEY_X:
+                keypad[0] = 0;
+                break;
+            case KEY_C:
+                keypad[0xB] = 0;
+                break;
+            case KEY_V:
+                keypad[0xF] = 0;
+                break;
+            }
         }
-    }
-    break;
+        break;
+        }
     }
     return running;
 }
@@ -211,10 +216,11 @@ void platform_update(struct window *window, uint8_t *display_buffer, uint16_t di
         window->width = wa.width;
         window->height = wa.height;
     }
+
     uint16_t x_scale = window->width / display_width;
     uint16_t y_scale = window->height / display_height;
 
-    /* Maintain 64:32 aspect ratio */
+    /* Maintain 2:1 aspect ratio */
     uint16_t min_scale = (x_scale < y_scale) ? x_scale : y_scale;
 
     for (uint8_t y = 0; y < display_height; y++)
