f83992ba68edca57e59a7dd22f2eeb9fa2b9b53a
Nael Iskandarani <nael.nanomano@gmail.com>
2023-11-07 23:30:45 -0600

    Change clock speed conversion to microseconds

--- src/main.c
+++ src/main.c
@@ -16,37 +16,43 @@ int main(int argc, char **argv)
     }
 
     uint16_t clock_speed = atoi(argv[1]);
-    double cpu_delay = (1.0 / clock_speed) * 1000;
+    uint64_t cycle_time = (1.0 / clock_speed) * 1000000;
     const char *filename = argv[2];
 
     struct chip8 chip8;
-    chip8_init(&chip8, 0x200);
+    chip8_init(&chip8, START_ADDRESS);
     chip8_load_rom(&chip8, filename);
 
     struct timespec current_time;
     clock_gettime(CLOCK_MONOTONIC, &current_time);
 
+    uint64_t acc = 0;
     chip8.running = 1;
     while (chip8.running)
     {
-        if (XPending(window.dpy))
-        {
-            XNextEvent(window.dpy, &window.e);
-            chip8.running = platform_process_input(&window, chip8.keypad);
-        }
+        chip8.running = platform_process_input(&window, chip8.keypad);
+
         struct timespec new_time;
         clock_gettime(CLOCK_MONOTONIC, &new_time);
 
         /* Calculate dt in milliseconds */
-        double dt =
-            (new_time.tv_sec - current_time.tv_sec) * 1000.0 + (new_time.tv_nsec - current_time.tv_nsec) / 1000000.0;
+        uint64_t dt =
+            (new_time.tv_sec - current_time.tv_sec) * 1000000 + (new_time.tv_nsec - current_time.tv_nsec) / 1000;
 
-        if (dt > cpu_delay)
+        if (dt > cycle_time)
         {
             current_time = new_time;
-
             chip8_cycle(&chip8);
 
+            if (chip8.delay_timer > 0)
+            {
+                acc += dt;
+                if (acc >= 1000000 / 60)
+                {
+                    chip8.delay_timer--;
+                    acc = 0;
+                }
+            }
             if (chip8.draw_flag)
             {
                 platform_update(&window, chip8.display, DISPLAY_WIDTH, DISPLAY_HEIGHT);
